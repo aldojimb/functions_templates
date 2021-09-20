76576a7c88a1697fe487f0c701bd120a3ad297d4
aldojimb <aldojimb@gmail.com>
2021-09-21 01:07:33 +0200

    'tercera entrega'

--- entrega3.js
+++ entrega3.js
@@ -1,23 +1,48 @@
 /*Nivell 1 - Exercici 1
 Crear una function que retorni una Promise que invoqui la funcion resolve() o bé reject() que rep. 
 Invocar-la des de fora pasandole totes dues funcions que imprimeixin un missatge diferent en cada cas.*/
+let parametro_1 = 1;
 
-const returnPromiseFunction = (x) => {
-    return new Promise((resolve, reject) =>{
-        if(x==1){
-        resolve('Success!');
-    } else {
-        reject('Try again');
-    }
-        }) ;
-    };
+ const generaPromesa = ()=> {
+     return new Promise((resolve, reject)=>{
+         if (parametro_1) {
+             resolve('Success! Se ha ejecutado el ejercicio 1 del nivel 1');
+         } else {
+             reject('Try again, change parametro_1 to 1');
+         }
+     })
+ };
 
-console.log(returnPromiseFunction(0));
-console.log(returnPromiseFunction(1));
+const handleSuccess = (resolvedValue)=> {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (resolvedFailure) => {
+    console.log(resolvedFailure);
+};
+
+ generaPromesa(parametro_1)
+    .then(handleSuccess)
+    .catch(handleFailure);
 
 /*- Exercici 2
 Crear una arrow function que, rebent un paràmetre i una function callback, li passi a la funció
  un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre.*/
+ const imprimir_Valor = resultadoFuncion => console.log(resultadoFuncion);
+
+  const funcionCallback = (parametro_2, functionParametro_2) => {
+    if (parametro_2){
+        valor_Parametro = `El parametro_1 es igual a ${parametro_2}`;
+    } else {
+        valor_Parametro= `El parametro_1 es distinto a 1. Su valor es ${parametro_2}`; 
+    }
+    functionParametro_2(valor_Parametro);
+ }
+
+ funcionCallback(0, imprimir_Valor);
+ funcionCallback(1, imprimir_Valor);
+
+
 
 
 /*Nivell 2 - Exercici 1
@@ -46,10 +71,37 @@ let salaries = [{
     salary: 2000
 }];
 
+const getEmpleado = (id_Objeto) => {
+    return new Promise((resolve, reject)=>{
+        if (id_Objeto >= 1 && id_Objeto <= 3) {
+            const elegido = employees.findIndex(empleado => empleado.id === id_Objeto);
+            console.log('Se ha encontrado al empleado')  
+            resolve(employees[elegido]); 
+        } else {
+            reject(`El índice empleado no es correcto. Solo puede tener valores del 1 al 3.
+            Cámbialo y vuelve a probar.`)
+        }
+    })
+}
+
+
+
 
 /*- Exercici 2
 Creu una altra arrow function getSalario que rebi com a paràmetre un objecte employee i retorni el seu salari.*/
 
+const getSalario = (id_Objeto_Employee) => {
+    return new Promise((resolve, reject)=>{
+        const elegido2 = salaries.findIndex(salario => salario.id === id_Objeto_Employee.id);  
+        if (elegido2 >= 0 && elegido2 <= salaries.length-1) {
+            console.log(`Se ha obtenido el salario de ${id_Objeto_Employee.name}`);
+            resolve(salaries[elegido2]); 
+        } else {
+            reject(`El nombre de empleado no tiene salario asigando.
+            El índice no existe.`)
+        }
+    })
+}
 
 /*- Exercici 3
 Invoqui la primera Promise getEmpleado i posteriorment getSalario, niant l'execució de les dues promises.*/
@@ -57,3 +109,30 @@ Invoqui la primera Promise getEmpleado i posteriorment getSalario, niant l'execu
 
 /*Nivell 3 - Exercici 1
 Fixi un element catch a la invocació de la fase anterior que capturi qualsevol error i l'imprimeixi per consola.*/
+
+getEmpleado(1)
+.then((res) => {
+    console.log(res.name) 
+})
+.catch((rejection) => {
+    console.log(rejection)
+});
+
+getSalario(employees[0])
+.then((res2) => {
+    console.log(res2.salary) 
+})
+.catch((err) => {
+    console.log(err)
+});
+
+getEmpleado(1)
+    .then((resolve1) => {
+        return getSalario(resolve1)
+    })
+    .then((resolve2) => {
+       console.log(`Salario ${resolve2.salary}`)
+    })
+    .catch((errormessage) => {
+        console.log(errormessage)
+    });
